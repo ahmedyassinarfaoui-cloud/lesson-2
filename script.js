75b67fb554dae6fd60cf62207d432ee7137335d4
ahmedyassinarfaoui-cloud <ahmedyassinarfaoui@gmail.com>
2026-09-01 20:43:15 +0200

    Remove checkAnswer function from script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-function checkAnswer(answer) {
 
-    const result = document.getElementById("result");
-
-    if (answer === 8) {
-        result.textContent = "✅ إجابة صحيحة!";
-    } else {
-        result.textContent = "❌ حاول مرة أخرى";
-    }
-
-}
